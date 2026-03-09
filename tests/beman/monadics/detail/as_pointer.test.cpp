5b075dfb7b21f7ebe647b9557488d3fe6ee39a08
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-09 20:54:05 +0100

    test(as_pointer): add missing tests

---
+++ tests/beman/monadics/detail/as_pointer.test.cpp
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/monadics/detail/as_pointer.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace beman::monadics::detail::tests {
+
+TEST_CASE("concept") {
+    STATIC_REQUIRE(std::same_as<decltype(as_pointer<int>), int* const>);
+    STATIC_REQUIRE(std::same_as<decltype(as_pointer<const int>), int* const>);
+    STATIC_REQUIRE(std::same_as<decltype(as_pointer<int&>), int* const>);
+    STATIC_REQUIRE(std::same_as<decltype(as_pointer<const int&>), int* const>);
+    STATIC_REQUIRE(std::same_as<decltype(as_pointer<int&&>), int* const>);
+}
+
+TEST_CASE("value") {
+    STATIC_REQUIRE(as_pointer<int> == nullptr);
+    STATIC_REQUIRE(as_pointer<double> == nullptr);
+}
+
+} // namespace beman::monadics::detail::tests
