bcc8c5143c3ba646ab29a7d79668c1a4dd5abc91
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-09 20:54:23 +0100

    test(decomposable): add missing tests

---
+++ tests/beman/monadics/detail/decomposable.test.cpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/monadics/detail/decomposable.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <optional>
+#include <utility>
+#include <vector>
+
+namespace beman::monadics::detail::tests {
+
+TEST_CASE("concept") {
+    STATIC_REQUIRE(decomposable<std::pair<int, double>, 1>);
+    STATIC_REQUIRE(decomposable<std::pair<int, double>, 2>);
+    STATIC_REQUIRE(decomposable<std::vector<int>, 1>);
+
+    STATIC_REQUIRE_FALSE(decomposable<int, 1>);
+    STATIC_REQUIRE_FALSE(decomposable<std::optional<int>, 2>);
+    STATIC_REQUIRE_FALSE(decomposable<std::pair<int, double>, 3>);
+}
+
+TEST_CASE("ignore-cvref") {
+    STATIC_REQUIRE(decomposable<const std::optional<int>&, 1>);
+    STATIC_REQUIRE(decomposable<std::optional<int>&&, 1>);
+}
+
+} // namespace beman::monadics::detail::tests
