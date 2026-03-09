b779c07ff654923c7f7ad5437360d5089dce697d
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-09 20:56:01 +0100

    test(instance_of): add missing tests

---
+++ tests/beman/monadics/detail/instance_of.test.cpp
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/monadics/detail/instance_of.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <optional>
+#include <utility>
+#include <vector>
+
+namespace beman::monadics::detail::tests {
+
+TEST_CASE("concept") {
+    STATIC_REQUIRE(instance_of<std::optional<int>, std::optional>);
+    STATIC_REQUIRE(instance_of<std::vector<int>, std::vector>);
+    STATIC_REQUIRE(instance_of<std::pmr::vector<int>, std::vector>);
+    STATIC_REQUIRE(instance_of<std::pair<int, double>, std::pair>);
+
+    STATIC_REQUIRE_FALSE(instance_of<int, std::optional>);
+    STATIC_REQUIRE_FALSE(instance_of<std::optional<int>, std::vector>);
+}
+
+} // namespace beman::monadics::detail::tests
