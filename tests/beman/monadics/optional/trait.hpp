c51c73032913dc6744852708d2529473129c798c
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-14 11:36:13 +0100

    refactor(tests/optional): remove redundant inline from trait
    Functions defined inside class definitions are implicitly inline.

--- tests/beman/monadics/optional/trait.hpp
+++ tests/beman/monadics/optional/trait.hpp
@@ -9,7 +9,7 @@
 
 template <typename T>
 struct beman::monadics::box_traits<std::optional<T>> {
-    [[nodiscard]] inline static constexpr auto error() noexcept { return std::nullopt; }
+    [[nodiscard]] static constexpr auto error() noexcept { return std::nullopt; }
 };
 
 #endif // BEMAN_MONADICS_OPTIONAL_TRAIT_HPP
