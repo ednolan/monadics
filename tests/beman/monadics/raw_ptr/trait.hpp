fbace22688483a4a00164924471b09430a567cb3
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-14 11:40:46 +0100

    refactor(tests/raw_ptr): remove redundant inline from trait
    Functions defined inside class definitions are implicitly inline.

--- tests/beman/monadics/raw_ptr/trait.hpp
+++ tests/beman/monadics/raw_ptr/trait.hpp
@@ -18,15 +18,15 @@ struct beman::monadics::box_traits<Box> {
     template <typename>
     using rebind_error = Box;
 
-    [[nodiscard]] inline static constexpr bool has_value(const Box box) noexcept { return static_cast<bool>(box); }
+    [[nodiscard]] static constexpr bool has_value(const Box box) noexcept { return static_cast<bool>(box); }
 
-    [[nodiscard]] inline static constexpr value_type& value(Box box) noexcept { return *box; }
+    [[nodiscard]] static constexpr value_type& value(Box box) noexcept { return *box; }
 
-    [[nodiscard]] inline static constexpr auto error() noexcept { return nullptr; }
+    [[nodiscard]] static constexpr auto error() noexcept { return nullptr; }
 
-    [[nodiscard]] inline static constexpr Box make(value_type& v) noexcept { return &v; }
+    [[nodiscard]] static constexpr Box make(value_type& v) noexcept { return &v; }
 
-    [[nodiscard]] inline static constexpr Box make_error() noexcept { return nullptr; }
+    [[nodiscard]] static constexpr Box make_error() noexcept { return nullptr; }
 };
 
 #endif // BEMAN_MONADICS_RAW_PTR_TRAIT_HPP
