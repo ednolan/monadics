8c233b779ab434630a891f453342f6c7f099bb67
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-14 11:26:01 +0100

    refactor(transform): remove redundant inline from operator|
    Functions defined inside class definitions are implicitly inline.

--- include/beman/monadics/detail/transform.hpp
+++ include/beman/monadics/detail/transform.hpp
@@ -19,7 +19,7 @@ struct transform_t {
         Fn fn;
 
         template <is_box Box, same_unqualified_as<action> A, typename Traits = get_box_traits<Box>>
-        [[nodiscard]] friend inline constexpr decltype(auto) operator|(Box&& box, A&& a) noexcept
+        [[nodiscard]] friend constexpr decltype(auto) operator|(Box&& box, A&& a) noexcept
             requires requires {
                 requires same_box<Box,
                                   typename Traits::template rebind<decltype(invoke_with_value(
