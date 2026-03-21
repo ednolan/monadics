db80ab106d27d7fb67c0ab490f062c0c2acfca14
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-21 21:40:18 +0100

    test(optional/and_then): add value category tests

--- tests/beman/monadics/optional/and_then.test.cpp
+++ tests/beman/monadics/optional/and_then.test.cpp
@@ -4,7 +4,9 @@
 
 #include "beman/monadics/detail/and_then.hpp"
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
+
+#include <helpers/move_only.hpp>
 
 namespace beman::monadics::tests {
 
@@ -32,18 +34,24 @@ TEST_CASE("with-value-variadic") {
     STATIC_REQUIRE(result.value() == 1);
 }
 
-template <typename Box, auto op, auto fn>
-concept monadic_invocable = requires(Box&& box) {
-    { box | op(fn) };
-};
-
-TEST_CASE("with-value-variadic2") {
-    STATIC_REQUIRE(monadic_invocable<std::optional<int>, and_then, [](int) { return std::optional{10}; }>);
-    STATIC_REQUIRE(!monadic_invocable<std::optional<int>, and_then, []() { return std::optional{10}; }>);
-    // constexpr auto result = std::optional<int>{10} | and_then([](int) { return 10; });
-    // STATIC_REQUIRE(std::same_as<decltype(result), const std::optional<std::size_t>>);
-    // STATIC_REQUIRE(result.has_value() == true);
-    // STATIC_REQUIRE(result.value() == 1);
+TEMPLATE_TEST_CASE_SIG(
+    "keep-value-category",
+    "",
+    ((typename Box, auto Fn, bool Expected), Box, Fn, Expected),
+    (
+        std::optional<int>&, [](int&) { return std::optional<int>{}; }, true),
+    (
+        std::optional<int>&, [](int&&) { return std::optional<int>{}; }, false),
+    (
+        std::optional<int>&&, [](int&&) { return std::optional<int>{}; }, true),
+    (
+        std::optional<int>&&, [](int&) { return std::optional<int>{}; }, false),
+    (
+        const std::optional<int>&, [](const int&) { return std::optional<int>{}; }, true),
+    (
+        const std::optional<int>&, [](int&) { return std::optional<int>{}; }, false),
+    (std::optional<helpers::MoveOnly>&, [](helpers::MoveOnly) { return std::optional<helpers::MoveOnly>{}; }, false)) {
+    STATIC_REQUIRE(and_thenable<Box, decltype(Fn)> == Expected);
 }
 
 } // namespace beman::monadics::tests
