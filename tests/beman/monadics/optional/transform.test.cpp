5d2c43b0cc2e72fc98adee810015b055f6861421
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-21 21:40:34 +0100

    test(optional/transform): add value category tests

--- tests/beman/monadics/optional/transform.test.cpp
+++ tests/beman/monadics/optional/transform.test.cpp
@@ -4,7 +4,7 @@
 
 #include "beman/monadics/detail/transform.hpp"
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
 namespace beman::monadics::tests {
 
@@ -19,6 +19,23 @@ TEST_CASE("with-nullopt") {
     constexpr auto result = std::optional<int>{} | transform([](auto&& value) { return (value - 1) * 0.5; });
     STATIC_REQUIRE(std::same_as<decltype(result), const std::optional<double>>);
     STATIC_REQUIRE(result.has_value() == false);
+}
+
+TEMPLATE_TEST_CASE_SIG("keep-value-category",
+                       "",
+                       ((typename Box, auto Fn, bool Expected), Box, Fn, Expected),
+                       (
+                           std::optional<int>&, [](int&) { return 0; }, true),
+                       (
+                           std::optional<int>&, [](int&&) { return 0; }, false),
+                       (
+                           std::optional<int>&&, [](int&&) { return 0; }, true),
+                       (
+                           std::optional<int>&&, [](int&) { return 0; }, false),
+                       (
+                           const std::optional<int>&, [](const int&) { return 0; }, true),
+                       (const std::optional<int>&, [](int&) { return 0; }, false)) {
+    STATIC_REQUIRE(transformable<Box, decltype(Fn)> == Expected);
 }
 
 } // namespace beman::monadics::tests
