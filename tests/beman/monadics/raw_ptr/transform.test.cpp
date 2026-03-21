dbe4f75b4b1db9e365a305803271928225ff066f
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-21 21:40:34 +0100

    test(raw_ptr/transform): add value category tests

--- tests/beman/monadics/raw_ptr/transform.test.cpp
+++ tests/beman/monadics/raw_ptr/transform.test.cpp
@@ -4,10 +4,19 @@
 
 #include "beman/monadics/detail/transform.hpp"
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
 namespace beman::monadics::tests {
 
 TEST_CASE("not-supported") {}
+
+TEMPLATE_TEST_CASE_SIG("value-is-always-lvalue-ref",
+                       "",
+                       ((typename Box, auto Fn, bool Expected), Box, Fn, Expected),
+                       (
+                           int*&, [](int&&) { return 0; }, false),
+                       (int*&&, [](int&&) { return 0; }, false)) {
+    STATIC_REQUIRE(transformable<Box, decltype(Fn)> == Expected);
+}
 
 } // namespace beman::monadics::tests
