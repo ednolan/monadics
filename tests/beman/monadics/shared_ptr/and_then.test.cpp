780e6180a8f02f463b72187cfbcf6cf756a0bbef
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-21 21:40:19 +0100

    test(shared_ptr/and_then): add value category tests

--- tests/beman/monadics/shared_ptr/and_then.test.cpp
+++ tests/beman/monadics/shared_ptr/and_then.test.cpp
@@ -4,7 +4,7 @@
 
 #include "beman/monadics/detail/and_then.hpp"
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
 namespace beman::monadics::tests {
 
@@ -34,6 +34,29 @@ TEST_CASE("with-value-return-member") {
         std::make_shared<Boo>() | and_then([](auto&& boo) { return std::forward<decltype(boo)>(boo).foo; });
     STATIC_REQUIRE(std::same_as<decltype(result), const std::shared_ptr<Foo>>);
     REQUIRE(static_cast<bool>(result) == false);
+}
+
+TEMPLATE_TEST_CASE_SIG("value-is-always-lvalue-ref",
+                       "",
+                       ((typename Box, auto Fn, bool Expected), Box, Fn, Expected),
+                       (
+                           std::shared_ptr<int>&, [](int&) { return std::shared_ptr<int>{}; }, true),
+                       (
+                           std::shared_ptr<int>&&, [](int&) { return std::shared_ptr<int>{}; }, true),
+                       (
+                           std::shared_ptr<int>&, [](int&&) { return std::shared_ptr<int>{}; }, false),
+                       (std::shared_ptr<int>&&, [](int&&) { return std::shared_ptr<int>{}; }, false)) {
+    STATIC_REQUIRE(and_thenable<Box, decltype(Fn)> == Expected);
+}
+
+TEST_CASE("pointee-is-mutable-through-lvalue-ref") {
+    auto sp     = std::make_shared<int>(5);
+    auto result = sp | and_then([](int& v) {
+                      v *= 3;
+                      return std::make_shared<int>(v);
+                  });
+    REQUIRE(*sp == 15);
+    REQUIRE(*result == 15);
 }
 
 } // namespace beman::monadics::tests
