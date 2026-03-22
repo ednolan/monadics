13f23b7d2a49f9d1a91f56debd3a3520496d6cbd
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-03-22 23:58:11 +0100

    feat(transform_error): enforce has_error_channel requirement

--- tests/beman/monadics/optional/transform_error.test.cpp
+++ tests/beman/monadics/optional/transform_error.test.cpp
@@ -2,12 +2,13 @@
 
 #include "trait.hpp"
 
-#include "beman/monadics/detail/transform_error.hpp"
-
 #include <catch2/catch_test_macros.hpp>
 
 namespace beman::monadics::tests {
 
-TEST_CASE("not-supported") {}
+TEST_CASE("not-supported") {
+    STATIC_REQUIRE_FALSE(
+        transform_errorable<std::optional<int>, decltype([](std::nullopt_t) { return std::nullopt; })>);
+}
 
 } // namespace beman::monadics::tests
--- tests/beman/monadics/shared_ptr/transform_error.test.cpp
+++ tests/beman/monadics/shared_ptr/transform_error.test.cpp
@@ -2,12 +2,12 @@
 
 #include "trait.hpp"
 
-#include "beman/monadics/detail/transform_error.hpp"
-
 #include <catch2/catch_test_macros.hpp>
 
 namespace beman::monadics::tests {
 
-TEST_CASE("not-supported") {}
+TEST_CASE("not-supported") {
+    STATIC_REQUIRE_FALSE(transform_errorable<std::shared_ptr<int>, decltype([](std::nullptr_t) { return nullptr; })>);
+}
 
 } // namespace beman::monadics::tests
