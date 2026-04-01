1fbfa29cd189d07dca74c13b6409584b20aaaab4
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-04-01 20:22:10 +0200

    refactor(transform): migrate to pipe_adaptor

--- include/beman/monadics/detail/transform.hpp
+++ include/beman/monadics/detail/transform.hpp
@@ -3,59 +3,47 @@
 #ifndef BEMAN_MONADICS_DETAIL_TRANSFORM_HPP
 #define BEMAN_MONADICS_DETAIL_TRANSFORM_HPP
 
+#include <beman/monadics/detail/get_box_traits.hpp>
 #include <beman/monadics/detail/invoke_with_value.hpp>
+#include <beman/monadics/detail/pipe_adaptor.hpp>
 #include <beman/monadics/detail/rebox_error.hpp>
-#include <beman/monadics/detail/and_then.hpp>
-#include "beman/monadics/detail/same_box.hpp"
+#include <beman/monadics/detail/same_box.hpp>
 
 #include <type_traits>
 #include <utility>
 
 namespace beman::monadics::detail {
 
-struct transform_t {
-    template <typename Fn>
-    struct action {
-        Fn fn;
+class transform_t {
+    inline static constexpr access_key<transform_t> key{};
 
-        template <is_box Box, same_unqualified_as<action> A, typename Traits = get_box_traits<Box>>
-        [[nodiscard]] friend constexpr decltype(auto) operator|(Box&& box, A&& a) noexcept
-            requires requires {
-                requires same_box<Box,
-                                  typename Traits::template rebind<decltype(invoke_with_value(
-                                      std::forward<A>(a).fn, std::forward<Box>(box)))>>;
-            }
-        /*
-            requires {
-              { box | and_then(...) } -> same_box2<Box>
-            }
-        */
-        {
-            using NewValue     = decltype(invoke_with_value(std::forward<A>(a).fn, std::forward<Box>(box)));
-            using NewBox       = typename Traits::template rebind<NewValue>;
-            using NewBoxTraits = get_box_traits<NewBox>;
-
-            // box | and_then
-            if (Traits::has_value(box)) {
-                if constexpr (std::is_void_v<NewValue>) {
-                    invoke_with_value(std::forward<A>(a).fn, std::forward<Box>(box));
-                    return NewBoxTraits::make();
-                } else {
-                    return NewBoxTraits::make(invoke_with_value(std::forward<A>(a).fn, std::forward<Box>(box)));
-                }
-            }
-
-            return rebox_error<NewBox>(std::forward<Box>(box));
+    template <is_box Box, std::derived_from<transform_t> Op, typename Traits = get_box_traits<Box>>
+    [[nodiscard]] friend constexpr decltype(auto) operator|(Box&& box, Op&& op)
+        requires requires {
+            requires same_box<Box,
+                              typename Traits::template rebind<decltype(invoke_with_value(
+                                  std::forward<Op>(op).callable(key), std::forward<Box>(box)))>>;
         }
-    };
+    {
+        using NewValue     = decltype(invoke_with_value(std::forward<Op>(op).callable(key), std::forward<Box>(box)));
+        using NewBox       = typename Traits::template rebind<NewValue>;
+        using NewBoxTraits = get_box_traits<NewBox>;
 
-    template <typename Fn>
-    [[nodiscard]] constexpr decltype(auto) operator()(Fn&& fn) const noexcept {
-        return action<decltype(fn)>{std::forward<Fn>(fn)};
+        if (Traits::has_value(box)) {
+            if constexpr (std::is_void_v<NewValue>) {
+                invoke_with_value(std::forward<Op>(op).callable(key), std::forward<Box>(box));
+                return NewBoxTraits::make();
+            } else {
+                return NewBoxTraits::make(
+                    invoke_with_value(std::forward<Op>(op).callable(key), std::forward<Box>(box)));
+            }
+        }
+
+        return rebox_error<NewBox>(std::forward<Box>(box));
     }
 };
 
-inline constexpr transform_t transform{};
+inline constexpr pipe_adaptor<transform_t> transform{};
 
 template <typename Box, typename Fn>
 concept transformable = requires(std::remove_reference_t<Box> box, std::remove_reference_t<Fn> fn) {
