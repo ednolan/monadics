6fbd2829dd6c5c5ee244bd5936ca19e37d68d7c3
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2026-04-07 19:40:00 +0200

    refactor(propagate_error): extract and extend concept

--- include/beman/monadics/detail/propagate_error.hpp
+++ include/beman/monadics/detail/propagate_error.hpp
@@ -11,12 +11,21 @@
 namespace beman::monadics::detail {
 
 template<typename NewBox, typename Box>
-    requires same_box<NewBox, Box>
+concept propagatable_error = same_box<NewBox, Box> && (requires {
+    requires has_error_channel<Box>;
+    { get_box_traits<NewBox>::make_error(get_box_traits<Box>::error(std::declval<Box>())) };
+} || requires {
+    requires !has_error_channel<Box>;
+    { get_box_traits<NewBox>::make_error(get_box_traits<Box>::error()) };
+});
+
+template<typename NewBox, typename Box>
+    requires propagatable_error<NewBox, Box>
 [[nodiscard]] constexpr decltype(auto) propagate_error(Box&& box) {
     using BoxTraits = get_box_traits<Box>;
     using NewBoxTraits = get_box_traits<NewBox>;
 
-    if constexpr (requires { BoxTraits::error(std::forward<Box>(box)); }) {
+    if constexpr (has_error_channel<Box>) {
         return NewBoxTraits::make_error(BoxTraits::error(std::forward<Box>(box)));
     } else {
         return NewBoxTraits::make_error(BoxTraits::error());
